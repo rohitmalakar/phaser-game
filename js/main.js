2355f21c94757a67af622c5be2af1bb141abf39a
rohitmalakar <rohitmalakar@gmail.com>
2017-03-22 18:03:50 +0545

    added sprite and movement.

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ function preload(){
 	game.load.image('sky', 'assets/sky.png');
 	game.load.image('ground', 'assets/ground.png');
 	game.load.image('star', 'assets/star.png');
-	game.load.image('dude', 'assets/dude.png', 32, 48);
+	game.load.spritesheet('dude', 'assets/player-sprite.png', 32, 56);
 }
 function create() {
 	// game.add.sprite(0, 0, 'star');
@@ -16,7 +16,7 @@ function create() {
 	// We are going to be using physics, so enable the arcade physics system
 	game.physics.startSystem(Phaser.Physics.ARCADE);
 
-	// A simple background for our game
+		// A simple background for our game
 	var sky = game.add.sprite(0, 0, 'sky');
 
 
@@ -48,8 +48,43 @@ function create() {
 	ledge.body.immovable = true;
 	
 
+	// The player and its settings
+	player = game.add.sprite(400, 450, 'dude');
+
+	// We need to enable physics on the player 
+	game.physics.arcade.enable(player);
+
+	player.body.setSize(32, 56, 0, -9);
+
+	// Player physics properties. Giv the little guy a slight bounce.
+	player.body.bounce.y = 0.2;
+	player.body.gravity.y = 500;
+	player.body.colliderWorldBounds = true;
+
+	// Our two animations, walking left and right
+	player.animations.add('left', [0, 1, 2, 3, 4, 5], 10, true);
+	player.animations.add('right', [6, 7, 8, 9, 10], 10, true);
 
 }
 function update() {
-	
+	// Collide the player and the stars with the platforms 
+	var hitPlatform = game.physics.arcade.collide(player, platforms);
+
+	// Rest the player velocity (movement)
+	player.body.velocity.x = 0;
+
+	// var cursors = game.input.keyboard.createCursorkeys();
+	var cursors = game.input.keyboard.createCursorKeys();
+
+	if(cursors.left.isDown){
+		// Move to the left
+		player.body.velocity.x = -150;
+		player.animations.play('left');
+	}else if (cursors.right.isDown) {
+		// Move to the right
+		player.body.velocity.x = +150;
+		player.animations.play('right');
+	}else if(cursors.up.isDown){
+		player.body.velocity.y = +150;
+	}
 }
